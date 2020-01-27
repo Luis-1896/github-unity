979587cf52e69ac3d60d75170a0c459efd2a0aed
luis1896 <luismartinezlopez1896@gmail.com>
2020-01-27 17:22:03 -0600

    Firebase co Unity

---
+++ Assets/Scripts/AuthManager.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AuthManager : MonoBehaviour
+{
+    protected Firebase.Auth.FirebaseAuth auth;
+    protected Firebase.Auth.FirebaseUser user;
+    private string displayName;
+
+    public InputField inputFieldEmail;
+    public InputField inputFieldPassword;
+
+    private bool signedIn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeFirebase();//aqui dara pauta para la inicializar
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (signedIn)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    void InitializeFirebase()
+    {
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth.StateChanged += AuthStateChanged;
+        AuthStateChanged(this, null);
+    }
+
+    void AuthStateChanged(object sender, System.EventArgs eventArgs)
+    {
+        if (auth.CurrentUser != user)
+        {
+            bool signedIn = user != auth.CurrentUser && auth.CurrentUser != null;
+            if (!signedIn && user != null)
+            {
+                Debug.Log("Signed out " + user.UserId);
+            }
+            user = auth.CurrentUser;
+            if (signedIn)
+            {
+                Debug.Log("Signed in " + user.UserId);
+                displayName = user.DisplayName ?? "";
+               // emailAddress = user.Email ?? "";
+                //photoUrl = user.PhotoUrl ?? "";
+            }
+        }
+    }
+
+    public void createUserByEmail()
+    {
+        string email = inputFieldEmail.text;
+        string password = inputFieldPassword.text;
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            // Firebase user has been created.
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+
+    }
+
+    public void ActivateSession()
+    {
+        Firebase.Auth.FirebaseAuth auth;
+        Firebase.Auth.FirebaseUser user;
+        void InitializeFirebase()
+        {
+            Debug.Log("Setting up Firebase Auth");
+            auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+            auth.StateChanged += AuthStateChanged;
+            AuthStateChanged(this, null);
+        }
+
+        void AuthStateChanged(object sender, System.EventArgs eventArgs)
+        {
+            if (auth.CurrentUser != user)
+            {
+                signedIn = user != auth.CurrentUser && auth.CurrentUser != null;
+                if (!signedIn && user != null)
+                {
+                    Debug.Log("Signed out " + user.UserId);
+                }
+                user = auth.CurrentUser;
+                if (signedIn)
+                {
+                    Debug.Log("Signed in " + user.UserId);
+                    //displayName = user.DisplayName ?? "";
+                    //emailAddress = user.Email ?? "";
+                    //photoUrl = user.PhotoUrl ?? "";
+                }
+            }
+        }
+
+        void OnDestroy()
+        {
+            auth.StateChanged -= AuthStateChanged;
+            auth = null;
+        }
+    }
+    
+    public void GetSessionProfiel()
+    {
+        Firebase.Auth.FirebaseUser user = auth.CurrentUser;
+        if (user != null)
+        {
+            string name = user.DisplayName;
+            string email = user.Email;
+            System.Uri photo_url = user.PhotoUrl;
+            // The user's Id, unique to the Firebase project.
+            // Do NOT use this value to authenticate with your backend server, if you
+            // have one; use User.TokenAsync() instead.
+            string uid = user.UserId;
+        }
+    }
+}
---
+++ Assets/Scripts/DBManager.cs
@@ -0,0 +1,76 @@
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DBManager : MonoBehaviour
+{
+    DatabaseReference reference;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set this before calling into the realtime database.
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://fir-unity-62bbe.firebaseio.com/");
+        // Get the root reference location of the database.
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+        InicializarBD();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void InicializarBD()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                //   app = Firebase.FirebaseApp.DefaultInstance;
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+    }
+
+    public void BTNGuardar()
+    {
+        writeNewUser("0004", "Armando", "armandrh@hotmail.com");
+    }
+
+    private void writeNewUser(string userId, string name, string email)
+    {
+        User user = new User(name, email);
+        string json = JsonUtility.ToJson(user);
+
+        reference.Child("users").Child(userId).SetRawJsonValueAsync(json);
+    }
+}
+
+public class User
+{
+    public string username;
+    public string email;
+
+    public User()
+    {
+    }
+
+    public User(string username, string email)
+    {
+        this.username = username;
+        this.email = email;
+    }
+}
